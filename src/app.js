836d4a1ef3fd87c267bfb45591156eba3693f570
Max <KontinentKM@gmail.com>
2024-10-09 10:16:46 +0300

    feat: implement navigation nwd.js

--- src/app.js
+++ src/app.js
@@ -1,63 +1,65 @@
-import readline from 'readline';
-import { handleCommand } from './commands/nwd.js';
-import { handleFileOperations } from './commands/fileOperations.js';
-import { handleOSInfo } from './commands/osInfo.js';
-import { calculateHash } from './commands/hash.js';
-import { handleCompression } from './commands/compress.js';
-import { printMessage, printCurrentDirectory } from './utils/printer.js';
+import readline from "readline";
+import { handleCommand } from "./commands/nwd.js";
+import { handleFileOperations } from "./commands/fileOperations.js";
+import { handleOSInfo } from "./commands/osInfo.js";
+import { calculateHash } from "./commands/hash.js";
+import { handleCompression } from "./commands/compress.js";
+import { printMessage, printCurrentDirectory } from "./utils/printer.js";
 
-const username = process.argv.find(arg => arg.startsWith('--username='))?.split('=')[1] || 'User';
+const username =
+  process.argv.find((arg) => arg.startsWith("--username="))?.split("=")[1] ||
+  "User";
 
 printMessage(`Welcome to the File Manager, ${username}!`);
 printCurrentDirectory(process.cwd());
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('line', async (input) => {
-    const [command, ...args] = input.trim().split(' ');
+rl.on("line", async (input) => {
+  const [command, ...args] = input.trim().split(" ");
 
-    try {
-        switch (command) {
-            case 'up':
-            case 'cd':
-            case 'ls':
-                await handleCommand(command, args);
-                break;
-            case 'cat':
-            case 'add':
-            case 'rn':
-            case 'cp':
-            case 'mv':
-            case 'rm':
-                await handleFileOperations(command, args);
-                break;
-            case 'os':
-                await handleOSInfo(args);
-                break;
-            case 'hash':
-                await calculateHash(args[0]);
-                break;
-            case 'compress':
-            case 'decompress':
-                await handleCompression(command, args);
-                break;
-            case '.exit':
-                rl.close();
-                break;
-            default:
-                printMessage('Invalid input');
-        }
-    } catch (error) {
-        console.error('Operation failed');
+  try {
+    switch (command) {
+      case "up":
+      case "cd":
+      case "ls":
+        await handleCommand(command, args);
+        break;
+      case "cat":
+      case "add":
+      case "rn":
+      case "cp":
+      case "mv":
+      case "rm":
+        await handleFileOperations(command, args);
+        break;
+      case "os":
+        await handleOSInfo(args);
+        break;
+      case "hash":
+        await calculateHash(args[0]);
+        break;
+      case "compress":
+      case "decompress":
+        await handleCompression(command, args);
+        break;
+      case ".exit":
+        rl.close();
+        break;
+      default:
+        printMessage("Invalid input");
     }
+  } catch (error) {
+    console.error("Operation failed");
+  }
 
-    printCurrentDirectory(process.cwd());
+  printCurrentDirectory(process.cwd());
 });
 
-rl.on('close', () => {
-    printMessage(`Thank you for using File Manager, ${username}, goodbye!`);
-    process.exit(0);
+rl.on("close", () => {
+  printMessage(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
 });
---
+++ src/commands/nwd.js
@@ -0,0 +1,28 @@
+import { resolve } from "path";
+import { readdir } from "fs/promises";
+import { printMessage } from "../utils/printer.js";
+
+export async function handleCommand(command, args) {
+  switch (command) {
+    case "up":
+      process.chdir("..");
+      break;
+    case "cd":
+      const newPath = resolve(process.cwd(), args[0]);
+      process.chdir(newPath);
+      break;
+    case "ls":
+      const files = await readdir(process.cwd(), { withFileTypes: true });
+      files
+        .sort(
+          (a, b) =>
+            a.isDirectory() - b.isDirectory() || a.name.localeCompare(b.name)
+        )
+        .forEach((file, i) => {
+          console.log(
+            `${i} | ${file.name} | ${file.isDirectory() ? "directory" : "file"}`
+          );
+        });
+      break;
+  }
+}
