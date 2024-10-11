1ae0f8b9b9d2718759965a178c9d4a4872d095fc
Max <KontinentKM@gmail.com>
2024-10-11 10:46:39 +0300

    feat: table

--- src/commands/nwd.js
+++ src/commands/nwd.js
@@ -1,6 +1,5 @@
 import { resolve } from "path";
 import { readdir } from "fs/promises";
-import { printMessage } from "../utils/printer.js";
 import { handleError } from "../utils/errorHandler.js";
 
 export async function handleCommand(command, args) {
@@ -15,18 +14,30 @@ export async function handleCommand(command, args) {
         break;
       case "ls":
         const files = await readdir(process.cwd(), { withFileTypes: true });
+
+        console.log(
+          `\n${"Index".padEnd(10)} | ${"Name".padEnd(30)} | ${"Type".padEnd(
+            15
+          )}`
+        );
+        console.log("-".repeat(60));
+
         files
           .sort(
             (a, b) =>
               a.isDirectory() - b.isDirectory() || a.name.localeCompare(b.name)
           )
           .forEach((file, i) => {
+            const type = file.isDirectory() ? "directory" : "file";
             console.log(
-              `${i} | ${file.name} | ${
-                file.isDirectory() ? "directory" : "file"
-              }`
+              `${String(i).padEnd(10)} | ${file.name.padEnd(
+                30
+              )} | ${type.padEnd(15)}`
             );
           });
+
+        console.log();
+
         break;
     }
   } catch (error) {
